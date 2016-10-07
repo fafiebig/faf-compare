3c9ea5e09a396e537d175f86cd8ef4d7fd5c0fb6
Frank Fiebig <frank.fiebig@exozet.com>
2016-10-07 11:26:20 +0200

    add plugins base files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # faf-compare
-Wordpress Plugin for Image Compare based on TwentyTwenty
+
+Wordpress Plugin for Image Compare based on TwentyTwenty.
+Show images before/after comparison of your WordPress images.
+
+# Installation
+
+* Unzip and upload /bootcamp-compare/ to the /wp-content/plugins/ directory
+* Activate the plugin through the plugins menu in WordPress
+* Use the editor button or [compare before="<id of image>" after="<id of image>"] to show comparison.
---
+++ twentytwenty/js/jquery.compare.js
@@ -0,0 +1,14 @@
+jQuery( document ).ready(function() {
+    jQuery('.twentytwenty-container[data-orientation!="vertical"]').twentytwenty({
+        default_offset_pct: 0.7
+    });
+
+    jQuery('.twentytwenty-container[data-orientation="vertical"]').twentytwenty({
+        default_offset_pct: 0.3,
+        orientation: 'vertical'
+    });
+
+    jQuery('.twentytwenty-container').height(jQuery('.twentytwenty-container img').height());
+    jQuery('.twentytwenty-container').width(jQuery('.twentytwenty-container img').width());
+});
+
